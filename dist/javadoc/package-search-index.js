fc9fb4bc0d7d1dc3c62603bc8607a7bbcc23dd04
unknown <narsineda@gmail.com>
2023-12-07 16:53:48 +0700

    first commit

---
+++ dist/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Bab1.ClassObject"},{"l":"Bab2.Constructors"},{"l":"Bab3.Inheritance"},{"l":"Bab4.Enkapsulasi"},{"l":"Bab5.OverridingOverloading"}];updateSearchResults();
